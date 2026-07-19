5822cdb7b489888a995748b44bb1950fc62fd84f
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2026-07-19 11:18:59 +0530

    header implementing

---
+++ src/components/Dashboard/EmployeDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./others/header";
+const EmployeDashboard=()=>{
+    return(
+        <div>
+            
+          <Header/>  
+        </div>
+    )
+}
+export default EmployeDashboard
---
+++ src/components/Dashboard/others/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Header =()=>{
+    return (
+        <div className="flex items-end justify-between">
+            <h1>hello <br/> Shubh</h1>
+            <button>Log out</button>
+        </div>
+    )
+}
+export default Header
