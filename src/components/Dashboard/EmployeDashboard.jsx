8c9b5e63146d2f5b3ea2f25adb4e93069dc78680
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2026-07-19 17:48:42 +0530

    employeDashboard done

--- src/components/Dashboard/EmployeDashboard.jsx
+++ src/components/Dashboard/EmployeDashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./others/header";
 import TaskListNumber from "./others/Tasklist";
+import TaskList from "../TaskList/TaskList";
 const EmployeDashboard=()=>{
     return(
         <>
           <Header/>  
           <TaskListNumber/>
+          <TaskList/>
         </>
     )
 }
