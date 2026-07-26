a118ceb95a419fbe235a83c4a0a00db880deeb4a
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2026-07-26 10:46:53 +0530

    refactor: update authentication context and improve login handling

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,16 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(null);
 
 
 const AuthProvider = ({ children }) => {
+  const [userData,setUserData]=useState(null)
+  useEffect(()=>{
+    const {employee,admin}= getLocalStorage()
+    setUserData({employee,admin})
+  })
   return (
-    <AuthContext.Provider value="shubh">
+    <AuthContext.Provider value={userData}>
       {children}
     </AuthContext.Provider>
   );
